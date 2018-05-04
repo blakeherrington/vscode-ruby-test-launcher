1470aaf06e68f540b0590852dd77b1a48db35fd8
Blake Herrington <blake.herrington@appfolio.com>
2018-05-04 15:54:39 -0700

    Add basic functionality
    Adds two commands - 'runTestLine' which will run a single test and 'runTestFile' which will run all tests in a file. Both leverage test launcher.

--- src/extension.ts
+++ src/extension.ts
@@ -4,18 +4,39 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    console.log('Lets go run ruby tests!');
-
     let runTestLine = vscode.commands.registerCommand('extension.runTestLine', () => {
-        vscode.window.showInformationMessage('Running test from current line...');
+        vscode.window.setStatusBarMessage('Test Launcher: Running test', 3000);
+
+        if (vscode.window.activeTextEditor !== undefined) {
+            const editor: vscode.TextEditor = vscode.window.activeTextEditor;
+
+            const fileName = editor.document.fileName;
+            const lineNumber = editor.selection.active.line;
+
+            issueCommand(`${fileName}:${lineNumber}`);
+        }
     });
 
     let runTestFile = vscode.commands.registerCommand('extension.runTestFile', () => {
-        vscode.window.showInformationMessage('Running all tests in current file...');
+        vscode.window.setStatusBarMessage('Test Launcher: Running all tests in file', 3000);
+
+        if (vscode.window.activeTextEditor !== undefined) {
+            const editor: vscode.TextEditor = vscode.window.activeTextEditor;
+
+            const fileName = editor.document.fileName;
+
+            issueCommand(fileName);
+        }
     });
 
     context.subscriptions.push(runTestLine);
     context.subscriptions.push(runTestFile);
+
+    function issueCommand(launcher_args: string): void {
+        const terminal: vscode.Terminal = vscode.window.createTerminal('Run Test File');
+        terminal.show();
+        terminal.sendText(`test_launcher ${launcher_args}`);
+    }
 }
 
 export function deactivate() {
